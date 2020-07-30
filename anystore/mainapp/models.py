1d88c0350e51a9fb6218587a1c9127ce77c456ac
AnryChe <stroyspecialist@rambler.ru>
2020-07-30 16:09:34 +0300

    migration

--- anystore/mainapp/models.py
+++ anystore/mainapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(verbose_name='название категории', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание категории', blank=True)
+
+    def __str__(self):
+        return self.name
